d295d29f50231146034312678a5303b812072a18
Web HM <133254421+webhm@users.noreply.github.com>
2023-05-24 14:24:06 -0500

    dev.25.4

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-
-// print_r(PDO::getAvailableDrivers());
-
-
-$db = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 172.16.3.247)(PORT = 1521)))(CONNECT_DATA=(SID=conclina)))";
-$c1 = oci_connect("mchang","1501508480",$db);
-print_r($c1);
+phpinfo();
 
 ?>
