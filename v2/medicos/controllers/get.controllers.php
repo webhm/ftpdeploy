4f4d3dedad65b068c3696cadc368b5f8f9ef16a3
martinxapps <52510496+martinxapps@users.noreply.github.com>
2023-05-27 18:09:27 -0500

    dev.27

--- v2/medicos/controllers/get.controllers.php
+++ v2/medicos/controllers/get.controllers.php
@@ -13,7 +13,7 @@ use app\models\medicos as Model;
 use Ocrend\Kernel\Helpers as Helper;
 
 $app->get('/', function () use ($app) {
-    return $app->json();
+    return $app->json(array());
 });
 
 # Resultados Imagen
