08a525b08f8f0bed95ef43fe4ac7136f553ce374
martinxapps <52510496+martinxapps@users.noreply.github.com>
2023-05-26 19:29:53 -0500

    dev.26.5

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 error_reporting(E_ALL ^ E_NOTICE);
 
 include('class.oracle.php');
+
 $ora = new ORACLE(); //::CreateInstance("XE", "olton", "yfnfkmz", "RUSSIAN_CIS.AL32UTF8"); 
 $ora->Connect("172.16.3.247:1521/conclina", "mchang", "1501508480");
 
